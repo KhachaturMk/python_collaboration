34db3b4f14581f4382ed0bb690dd5b2e98c20281
Khachatur Mkrtchyan <kmkrtchyan1986@gmail.com>
2024-05-12 13:43:04 +0400

    upd loan_calculator logs

--- loan.py
+++ loan.py
@@ -11,8 +11,7 @@ def loan_calculator(db):
     key = db.get(acc_number)
     if key is not None and loan_amount.isdigit() and int(loan_amount) > 0:
         loan_amount = int(loan_amount)
-        amount_to_be_paid = loan_amount + LOAN_INTEREST*loan_amount
-        print(f"\nYour annual payment will be {amount_to_be_paid} GEL")
+        print(f"\nOur interest is {LOAN_INTEREST*100} % per year")
 
         answer = input('Do you want to take a loan? (y/n): ')
         if answer == 'y':
@@ -28,7 +27,7 @@ def loan_calculator(db):
         monthly_payment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate)**LOAN_TERM_MONTHS) / ((1 + monthly_interest_rate)**LOAN_TERM_MONTHS - 1)
 
 
-        csvfile = open('loan_payment_plan.csv', mode='w', newline='')
+        csvfile = open('loan_payment_plan.csv', mode='a', newline='')
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(['Month', 'Starting Balance', 'Interest Payment', 'Principal Payment', 'Ending Balance'])
 
@@ -40,13 +39,13 @@ def loan_calculator(db):
 
             csvwriter.writerow([f'{month:<3}', f'{loan_amount: >15.2f}', f'{interest_payment: >15.2f}', f'{principal_payment: >15.2f}', f'{loan_amount - principal_payment: >15.2f}'])
             loan_amount -= principal_payment
-        csvwriter.writerow(['Total Amount to be Paid', f'{amount_to_be_paid:.2f}'])
+        csvwriter.writerow(['Total Amount to be Paid', f'{total_paid:.2f}'])
 
         csvfile.close()
 
       
 
-        print(f"Total amount to be paid over {LOAN_TERM_MONTHS} months: {amount_to_be_paid:.2f} GEL")
+        print(f"Total amount to be paid over {LOAN_TERM_MONTHS} months: {total_paid:.2f} GEL")
 
 
     else:
